dcdcd9a46908084c880c0bbbbadc44f6ce1357a2
NeoAtlantis <yet_another_email@126.com>
2013-07-31 00:44:38 +0800

    init a codebook manager

---
+++ codebook.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Codebook Management
+===================
+
+Description
+-----------
+This program implements all necessary operations in managing codebooks.
+Codebook is the core of our cipher system, which contains a lot of information
+that can be used to make up keys for symmetric ciphers.
+
+Codebooks are managed within the core command system. Exchanging codebooks,
+however, can be done by external plugins, e.g. asymmetric cipher, quantum
+communication, or direct cable connection to a random number generator
+cooperating with another cipher device.
+
+Therefore here codebooks are not generated. They can be added, with details
+and description and validity time period. They can be deleted. They can be
+queried, resulting in an access ID, descriptions, etc. The credentials in
+a codebook will not be revealed, and can be only used by other core commands
+to encrypt or decrypt messages.
+"""
+import os
+import sys
+
+class codebook_manager:
+
+    def __init__(self, path_to_database):
+        pass
+
+if __name__ == '__main__':
+    pass
