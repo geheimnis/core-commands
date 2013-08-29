171dfb9e758b22e3bf8c89789c73990535805478
NeoAtlantis <yet_another_email@126.com>
2013-08-30 00:32:21 +0800

    time display

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import sys
 import os
 import time
+import datetime
+
+dt = datetime.datetime.now()
 
 print "***** Arrival of Core Command Region ***"
 print "SYS.ARGV:"
 print repr(sys.argv), '\n'
-print "NOW:", time.time()
+print "MY PATH:"
+print os.path.realpath(os.path.dirname(sys.argv[0])), "\n"
+print "NOW: %.6f" % (time.mktime(dt.timetuple()) + dt.microsecond / 1000000.0)
 print "*" * 40
