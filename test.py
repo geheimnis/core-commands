d39226ccee78ffc13409474807c352eb49bdd59c
NeoAtlantis <yet_another_email@126.com>
2013-08-30 00:10:10 +0800

    time stamp

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import sys
 import os
+import time
 
 print "***** Arrival of Core Command Region ***"
 print "SYS.ARGV:"
-print repr(sys.argv)
+print repr(sys.argv), '\n'
+print "NOW:", time.time()
 print "*" * 40
