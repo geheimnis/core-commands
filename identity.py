03d6f0d549fba82b17a9465ef08fc53fd7a3546c
NeoAtlantis <yet_another_email@126.com>
2013-09-04 10:33:26 +0800

    removed contact and recognize item, due to new decision in PKI.

--- identity.py
+++ identity.py
@@ -19,7 +19,6 @@ HEX-Encoded string not longer than 128 characters.
     add     Examine and insert a new identity, which takes [ARGUMENTS] as
             configuration of this new identity.
     delete  [ARGUMENTS] here is the target identity's ID.
-    consult [ARGUMENTS] may be 'contact' or 'recognize'.
 
 Description
 -----------
@@ -30,7 +29,7 @@ from hash import object_hasher
 
 class identity:
 
-    _title, _describe, _contact, _recognize = '','',{},{}
+    _title, _describe = '',''
 
     _loaded = False
 
@@ -38,7 +37,7 @@ class identity:
         if init_string != None:
             self.load_string(init_string)
 
-    def initialize(self, title, describe, **argv):
+    def initialize(self, title, describe):
         """Create a new identity.
 
         Arguments:
@@ -46,55 +45,24 @@ class identity:
         title       size: [5, 30], a title of this new identity.
         describe    size: [0, 140], a brief description.
 
-        argv        with following possibilities:
-            contact     dict(associative array), with recognized keys as
-                        listed:
-                            phone, mobile, fax, email, addr,
-                            im/qq, im/xmpp, im/msn, im/icq, im/gtalk, im/aim,
-                            im/irc, im/yy, im/fetion, im/alibaba,
-                            web/site, web/baidu, web/facebook, web/twitter,
-                            web/googleplus, web/sinaweibo,
-                            other
-            recognize   dict(associative array), with recognized keys as
-                        listed:
-                            id/card, id/passport
-
-                    note that for each associative array, with given key user
-                    can specify an array of multiple values. Each value have
-                    same limits in length: [1,256].
-
         All above item values are limited to a charset with:
             a-z A-Z 0-9 _@#/\.:=+
         """
        
-        test_result = self._test_data(title, describe, **argv)
+        test_result = self._test_data(title, describe)
         if test_result == False: return False
 
-        self._title, self._describe, self._contact, self._recognize =\
-            test_result
+        self._title, self._describe = test_result
         self._loaded = True
 
-    def _test_data(self, title, describe, **argv):
+    def _test_data(self, title, describe):
         passed = (
             (self._filter_string(title) in xrange(5,31)) and
             (self._filter_string(describe) in xrange(0,141))
         )
 
-        contact, recognize = {}, {}
-
-        if argv.has_key('contact'):
-            passed &= self._filter_dict(
-                argv['contact'],
-                self.get_contact_methods()
-            )
-
-        if argv.has_key('recognize'):
-            passed &= self._filter_dict(
-                argv['recognize'],
-                self.get_recognize_methods()
-            )
         if passed == True:
-            return title, describe, contact, recognize
+            return title, describe
         else:
             return False
 
@@ -102,15 +70,10 @@ class identity:
         try:
             title = string['title']
             describe = string['describe']
-            contact, recognize = {}, {}
-            if string.has_key('contact'): contact = string['contact']
-            if string.has_key('recognize'): contact = string['recognize']
 
             self.initialize(
                 title,
                 describe,
-                contact=contact,
-                recognize=recognize
             )
 
         except:
@@ -124,27 +87,12 @@ class identity:
                 'title': self._title,
                 'describe': self._describe,
             }
-            if self._contact: ret['contact'] = self._contact
-            if self._recognize: ret['recognize'] = self._recognize
             return ret
 
     def get_id(self):
         if not self._loaded: return
         hasher = object_hasher('SHA-1')
         return hasher.hash(self.__str__()).encode('hex')
-
-    def _filter_dict(self, dictobj, possible_keys):
-        for each in dictobj:
-            if not each in possible_keys:
-                return False
-            if type(dictobj[each]) == str:
-                dictobj[each] = [dictobj[each],]
-            elif type(dictobj[each]) != list:
-                return False
-            for item in dictobj[each]:
-                if not self._filter_string(item) in xrange(1,257):
-                    return False
-        return True
 
     def _filter_string(self, string):
         if type(string) != str:
@@ -157,21 +105,6 @@ class identity:
             return len(string)
         else:
             return None
-
-    def get_contact_methods(self):
-        return [i.strip() for i in """
-            phone, mobile, fax, email, addr,
-            im/qq, im/xmpp, im/msn, im/icq, im/gtalk, im/aim,
-            im/irc, im/yy, im/fetion, im/alibaba,
-            web/site, web/baidu, web/facebook, web/twitter,
-            web/googleplus, web/sinaweibo,
-            other
-        """.split(',')]
-
-    def get_recognize_methods(self):
-        return [i.strip() for i in """
-            id/card, id/passport
-        """.split(',')]
 
 if __name__ == '__main__':
     import sys
@@ -212,22 +145,6 @@ if __name__ == '__main__':
                 result.keys()
             )
         )
-
-    elif operand == 'consult':
-        if argument == 'contact':
-            output.result(
-                json.dumps(
-                    identity().get_contact_methods()
-                )
-            )
-        elif argument == 'recognize':
-            output.result(
-                json.dumps(
-                    identity().get_recognize_methods()
-                )
-            )
-        else:
-            output.error('Consult failed.', 400)
 
     elif operand == 'delete':
         database.remove('identities', argument)
