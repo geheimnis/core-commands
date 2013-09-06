5051edb721e93d90c89ef1f813ddc234d31fcffe
NeoAtlantis <yet_another_email@126.com>
2013-09-06 22:26:49 +0800

    modified invoking mechanism

--- invoke.py
+++ invoke.py
@@ -17,8 +17,13 @@ python invoke.py <USER-IDENTIFIER> <DATABASE-ACCESS-KEY> <OPERAND> [ARGUMENTS]
 <OPERAND> may be 'run', 'query', or 'optimize'.
 
 When <OPERAND> is 'query', [ARGUMENTS] is a string of Result-ID.
+
 When <OPERAND> is 'run', [ARGUMENTS] are instructions of running an actual
-command.
+command. This is a string of JSON format, for example:
+    
+    {"cmd": "identity-list", "arg": None}
+
+
 When <OPERAND> is 'optimize', [ARGUMENTS] will be ignored. Old results, which
 is older as 60 seconds, will be deleted. Currently we do not put this number
 into config file.
@@ -28,6 +33,18 @@ Arguments <DATABASE-ACCESS-KEY>, [ARGUMENTS] are encoded in HEX.
 
 CONFIG_TABLE = 'invoke/process_result'
 CONFIG_RESULT_LIFE = 60
+
+CONFIG_COMMANDS = {
+    'instant': {
+        'identity-list': {'domain': 'identity', 'operand': 'list', 'arg': False},
+        'identity-test': {'domain': 'identity', 'operand': 'test', 'arg': True},
+        'identity-delete': {'domain': 'identity', 'operand': 'delete', 'arg': True},
+        'identity-add': {'domain': 'identity', 'operand': 'add', 'arg': True},
+    },
+    'wait': {
+
+    },
+}
 
 if __name__ == '__main__':
     import subprocess
@@ -72,23 +89,56 @@ if __name__ == '__main__':
 
     elif operand == 'run':
         try:
-            arguments = arguments_raw.decode('hex')
+            arguments = json.loads(arguments_raw.decode('hex'))
+            command_name = arguments['cmd'].lower()
+            command_argv = arguments['arg']
+
+            command_profile = {}
+            execute_instantly = False
+            success_code = 0
+            
+
+            if command_name in CONFIG_COMMANDS['instant']:
+                execute_instantly = True
+                command_profile = CONFIG_COMMANDS['instant'][command_name]
+
+            elif command_name in CONFIG_COMMANDS['wait']:
+                execute_instantly = False
+                command_profile = CONFIG_COMMANDS['wait'][command_name]
+
+            else:
+                raise Exception()
         except:
             output.error('Invalid argument.')
             exit()
 
-        new_id = get_uuid(arguments)
-        subprocess.Popen(\
-            [\
-                'python',
-                os.path.join(base_path, '_invoke_.py'),
-                user_identifier,
-                db_access_key_hex,
-                new_id,
-                arguments_raw,
-            ]\
-        )
-        output.result(new_id, 200)
+        execute_list = [
+            'python',
+            os.path.join(base_path, command_profile['domain'] + '.py'),
+            user_identifier,
+            db_access_key_hex,
+            command_profile['operand'],
+        ]
+        if command_profile['arg']:
+            execute_list.append(command_argv.split(' '))
+
+        if execute_instantly:
+            print subprocess.check_output(execute_list)
+
+        else:
+            new_id = get_uuid(arguments_raw)
+            new_arguments = ' '.join(execute_list).encode('hex')
+            subprocess.Popen(\
+                [\
+                    'python',
+                    os.path.join(base_path, '_invoke_.py'),
+                    user_identifier,
+                    db_access_key_hex,
+                    new_id,
+                    new_arguments,
+                ]\
+            )
+            output.result(new_id, 202)
 
     elif operand == 'optimize':
         data_list = database.get(CONFIG_TABLE)
